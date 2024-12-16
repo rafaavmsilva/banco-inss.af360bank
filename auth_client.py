11e3c70088952a5664f1a8be77e99353bcbdd6bb
rafaavmsilva <rafaavmsilva@gmail.com>
2024-12-16 15:11:57 -0300

    fix: Add get_authorization_url method to AuthClient

--- auth_client.py
+++ auth_client.py
@@ -1,11 +1,17 @@
 import requests
 from flask import session, redirect, request
+from urllib.parse import quote
 
 class AuthClient:
     def __init__(self, auth_server_url, app_name):
         self.auth_server_url = auth_server_url
         self.app_name = app_name
 
+    def get_authorization_url(self, redirect_uri):
+        """Generate the authorization URL for the auth server"""
+        encoded_redirect = quote(redirect_uri)
+        return f"{self.auth_server_url}/login?redirect_uri={encoded_redirect}&app_name={self.app_name}"
+    
     def init_app(self, app):
         self.app = app
 
